4fc0cb48d5d26435c4d735534712eb1e57475c87
Ruchi Tanmay <90126131+RuchiTanmay@users.noreply.github.com>
2026-04-18 19:01:06 +0530

    Update __init__.py
    version change

--- bseindia/__init__.py
+++ bseindia/__init__.py
@@ -2,4 +2,4 @@ from .libutil import all_listed_securities, trading_holiday_calendar
 from .get_data import historical_stock_data, equity_bhav_copy, derivative_bhav_copy, stock_info
 
 
-__version__ = "0.1+forkfix"
+__version__ = "0.1"
